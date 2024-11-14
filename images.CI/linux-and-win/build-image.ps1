d3c1b1c3346c2ce868154359671235593e1fe04c
Sasha Honerkamp <asmhonerkamp@gmail.com>
2024-11-13 17:42:39 -0800

    Update build-image.ps1

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -43,19 +43,19 @@ Write-Host "Validate packer template"
 packer validate -syntax-only $TemplatePath
 
 Write-Host "Build $ImageTemplateName VM"
-packer build    -var client_id='"$ClientId"' `
-                -var client_secret='"$ClientSecret"' `
-                -var install_password='"$InstallPassword"' `
-                -var location='"$Location"' `
-                -var managed_image_name='"$ImageName"' `
-                -var managed_image_resource_group_name='"$ImageResourceGroupName"' `
-                -var subscription_id='"$SubscriptionId"' `
-                -var temp_resource_group_name='"$TempResourceGroupName"' `
-                -var tenant_id='"$TenantId"' `
-                -var virtual_network_name='"$VirtualNetworkName"' `
-                -var virtual_network_resource_group_name='"$VirtualNetworkRG"' `
-                -var virtual_network_subnet_name='"$VirtualNetworkSubnet"' `
-                -var allowed_inbound_ip_addresses='"$($AllowedInboundIpAddresses)"' `
+packer build    -var "client_id=$ClientId" `
+                -var "client_secret=$ClientSecret" `
+                -var "install_password=$InstallPassword" `
+                -var "location=$Location" `
+                -var "managed_image_name=$ImageName" `
+                -var "managed_image_resource_group_name=$ImageResourceGroupName" `
+                -var "subscription_id=$SubscriptionId" `
+                -var "temp_resource_group_name=$TempResourceGroupName" `
+                -var "tenant_id=$TenantId" `
+                -var "virtual_network_name=$VirtualNetworkName" `
+                -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
+                -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
+                -var "allowed_inbound_ip_addresses=$($AllowedInboundIpAddresses)" `
                 -color=false `
                 $TemplatePath `
         | Where-Object {
